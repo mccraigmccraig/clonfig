c2ba3a18460cf314576bddbe2ea794ba0a649357
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-03-28 11:34:56 +0100

    preparing for travis-ci

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clonfig "0.1.1"
   :description "simple environment variable based config for clojure apps"
   :url "https://github.com/mccraigmccraig/clonfig"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [midje "1.2.0"]
-                     [lein-midje "1.0.3"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [
+                     [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [lein-midje "1.0.9"]])
 
